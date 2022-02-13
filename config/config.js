9b56b41ff2a23e407b635a6977e0bce65f66517c
neojibaros <jibarodao@protonmail.com>
2022-02-13 12:59:28 -0400

    add matic

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     app_name: 'Cool Rarity',
     app_description: 'Cool Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
     collection_file_name: 'collection.json',
-    collection_contract_address: '0x1b1f33a9eef47565d195e1943845aac1777312b8',
+    collection_contract_address: '/matic/0x1b1f33a9eef47565d195e1943845aac1777312b8',
     collection_name: 'Jibaros',
     collection_description: '10k "One Day I\'ll Be A Punk"-punks – a homage to the one and only CryptoPunks. Holding a OneDayPunk will give you early access to PunkScapes and reserve a profile on the PunkScape website.',
     collection_id_from: 1,
